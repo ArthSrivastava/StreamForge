a971e8a2a16018730def6534029d9e46898e918d
ArthSrivastava <arth4701@gmail.com>
2025-03-09 19:04:32 +0530

    fix: s3 bucket name underscore fix; chore: dev profile configs include s3 path style url for localstack

--- VideoUploader/src/main/java/com/example/videouploader/common/AppConstants.java
+++ VideoUploader/src/main/java/com/example/videouploader/common/AppConstants.java
@@ -1,5 +1,5 @@
 package com.example.videouploader.common;
 
 public class AppConstants {
-    public static final String S3_BUCKET_NAME = "raw_videos";
+    public static final String S3_BUCKET_NAME = "raw-videos";
 }
--- VideoUploader/src/main/java/com/example/videouploader/video/VideoUploaderController.java
+++ VideoUploader/src/main/java/com/example/videouploader/video/VideoUploaderController.java
@@ -4,15 +4,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/videos")
 @RequiredArgsConstructor
 @Slf4j
+@CrossOrigin(origins = "http://localhost:5173")
 class VideoUploaderController {
     private final VideoUploaderService videoUploaderService;
 
