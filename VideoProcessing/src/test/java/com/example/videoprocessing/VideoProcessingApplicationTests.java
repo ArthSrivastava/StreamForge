54da9c1caea3bf95c4642a667de7ab1789d03cff
ArthSrivastava <arth4701@gmail.com>
2025-03-10 22:41:09 +0530

    chore: video processing project init

---
+++ VideoProcessing/src/test/java/com/example/videoprocessing/VideoProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.videoprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
